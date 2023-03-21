84a1297db0d96354734ef0e2ee8049365113b2ae
stixbunny <milenko.castillo@gmail.com>
2023-03-21 18:44:26 -0300

    Update myApp.js - sendFile index

--- myApp.js
+++ myApp.js
@@ -1,7 +1,10 @@
 let express = require('express');
 let app = express();
 console.log("Hello World");
-app.get("/", (req, res) => {
+/*app.get("/", (req, res) => {
 				res.send("Hello Express");
+});*/
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + '/views/index.html');
 });
 module.exports = app;
