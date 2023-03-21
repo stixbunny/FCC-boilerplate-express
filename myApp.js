5a13f93c9ededc546711ec820c5f6d5649bfe237
stixbunny <milenko.castillo@gmail.com>
2023-03-21 18:49:52 -0300

    Update myApp.js - /public middleware

--- myApp.js
+++ myApp.js
@@ -2,9 +2,10 @@ let express = require('express');
 let app = express();
 console.log("Hello World");
 /*app.get("/", (req, res) => {
-				res.send("Hello Express");
+	res.send("Hello Express");
 });*/
 app.get("/", (req, res) => {
 	res.sendFile(__dirname + '/views/index.html');
 });
+app.use(express.static(__dirname + '/public'));
 module.exports = app;
