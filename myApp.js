59b2ff8b526fa1231f80ef4afd0ad181e64455db
stixbunny <milenko.castillo@gmail.com>
2023-03-21 18:54:33 -0300

    Update myApp.js - swap order

--- myApp.js
+++ myApp.js
@@ -4,8 +4,8 @@ console.log("Hello World");
 /*app.get("/", (req, res) => {
 	res.send("Hello Express");
 });*/
+app.use(express.static(__dirname + '/public'));
 app.get("/", (req, res) => {
 	res.sendFile(__dirname + '/views/index.html');
 });
-app.use(express.static(__dirname + '/public'));
 module.exports = app;
